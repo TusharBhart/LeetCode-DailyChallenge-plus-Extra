2e42e3ba6a1a509c1090004a811b6dfb892cbf6f
Tushar Bhart <121730758+TusharBhart@users.noreply.github.com>
2023-01-03 12:51:38 +0530

    Add files via upload

---
+++ January23/944-Delete-Columns-to-Make-Sorted.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size(), m = strs[0].size(), ans = 0;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n-1; j++) {
+                if(strs[j][i] > strs[j + 1][i]) {ans++; break;}
+            }
+        }
+        return ans;
+    }
+};
