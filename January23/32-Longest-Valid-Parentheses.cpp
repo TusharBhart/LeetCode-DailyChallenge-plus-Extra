1b4eb35d4c09cbd2cb4adf95a39f3fe2376fb96a
Tushar Bhart <121730758+TusharBhart@users.noreply.github.com>
2023-01-06 16:07:33 +0530

    Add files via upload

---
+++ January23/32-Longest-Valid-Parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int longestValidParentheses(string str) {
+        stack<int> s;
+        s.push(-1);
+        int ans = 0, n = str.size();
+
+        for(int i=0; i<n; i++) {
+            if(s.top() != -1 && str[s.top()] == '(' && str[i] == ')') s.pop(), ans = max(ans, i - s.top());
+            else s.push(i);
+        }
+        return ans;
+    }
+};
