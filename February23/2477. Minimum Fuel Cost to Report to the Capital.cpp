f40b11eac406b96598096a0e99f32616bafda88c
Tushar Bhart <121730758+TusharBhart@users.noreply.github.com>
2023-02-12 22:27:22 +0530

    Add files via upload

---
+++ February23/2477. Minimum Fuel Cost to Report to the Capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int dfs(int node, int prnt, vector<int> adj[], int seats, long long &ans) {
+        int cnt = 1;
+        for(int ad : adj[node]) {
+            if(ad != prnt) cnt += dfs(ad, node, adj, seats, ans);
+        }
+        if(node) ans += ceil((double) cnt / seats);
+        return cnt;
+    }
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        long long n = roads.size() + 1, ans = 0;
+        vector<int> adj[n];
+        for(auto e : roads) adj[e[0]].push_back(e[1]), adj[e[1]].push_back(e[0]);
+
+        dfs(0, -1, adj, seats, ans);
+        return ans;
+    }
+};
